8ead91d1a2a7af90a262bccc7f2bb6cd36d1de46
Natalia Balashova <88karamelka88@mail.ru>
2023-08-02 14:11:27 +0300

    add active

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,17 @@
-const slides = document.querySelectorAll(".slide");
-for(const slide of slides){
-    slide.addEventListener('click', () => {
-        clearActiveClasses();
-        slide.classList.add('active');
-    })
+function slidesPlugin(activeSlide){
+    const slides = document.querySelectorAll(".slide");
+
+    slides[activeSlide].classList.add('active');
+    for(const slide of slides){
+        slide.addEventListener('click', () => {
+            clearActiveClasses();
+            slide.classList.add('active');
+        })
+    }
+    function clearActiveClasses(){
+        slides.forEach((slide) => {
+            slide.classList.remove('active');
+        })
+    }
 }
-function clearActiveClasses(){
-    slides.forEach((slide) => {
-        slide.classList.remove('active');
-    })
-}
+slidesPlugin(2);
